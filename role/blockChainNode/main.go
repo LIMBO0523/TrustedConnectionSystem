ad863cd5f3383f7a9b58c0351be68f4d48acfbc8
limbo0523 <1132948658@qq.com>
2024-06-27 17:34:26 +0800

    部署合约功能

--- role/blockChainNode/main.go
+++ role/blockChainNode/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"TrustedConnectionSystem/measure"
+	"TrustedConnectionSystem/router"
 	"fmt"
+	"log"
 )
 
-func main() {
+func WatchVerifyAndStoreHashes() {
 	ivc := measure.GetIntergrityVerify()
 	fmt.Println("开始监听事件HashArrayStored")
 	ivc.WatchVerifyAndStoreHashes()
 	fmt.Println("事件HashArrayStored监听结束")
+}
+func main() {
+	go WatchVerifyAndStoreHashes()
+	r := router.BlockChainNodeRouter()
+	err := r.Run("8082")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
